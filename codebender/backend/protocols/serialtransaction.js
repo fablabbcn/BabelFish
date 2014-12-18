82e6558a3e84eb5fa08b9950a315bd4880f78ee9
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2014-12-18 16:13:10 +0200

    Untested read write bytes.

--- codebender/backend/protocols/serialtransaction.js
+++ codebender/backend/protocols/serialtransaction.js
@@ -1,7 +1,20 @@
 var _create_chrome_client = require('./../../../chrome-extension/client/rpc-client'),
     Transaction = require('./../transaction').Transaction,
     arraify = require('./../util').arraify,
-    buffer = require("./../buffer.js");
+    MemoryOperations = require('./memops');
+buffer = require("./../buffer.js");
+
+// Callback gets the next iteration as first
+function poll (maxRetries, timeout, cb) {
+  if (maxRetries < 0)
+    throw Error("Retry limit exceeded");
+
+  cb(function () {
+    setTimeout(function () {
+      poll(cb, maxRetries-1, timeout)
+    }, timeout || 50);
+  });
+}
 
 function SerialTransaction () {
   Transaction.apply(this, arraify(arguments));
@@ -15,9 +28,8 @@ function SerialTransaction () {
   this.log.log("Listening on buffer");
   this.serial.onReceive.addListener(this.listenerHandler);
 
-  this.memOps = {
-    CHIP_ERASE_ARR: [0xAC, 0x80, 0x00, 0x00]
-  };
+  this.memOps = new MemoryOperations();
+  this.memOps.CHIP_ERASE_ARR = [0xAC, 0x80, 0x00, 0x00];
 }
 SerialTransaction.prototype = new Transaction();
 
@@ -28,18 +40,16 @@ SerialTransaction.prototype.writeThenRead_ = function (outgoingMsg, responsePayl
 
   // schedule a read in 100ms
   this.serial.send(this.connectionId, outgoingBinary, function(writeArg) {
-    self.consumeMessage(responsePayloadSize, callback, function (connId) {
-      self.log.log("Disconnecting from", connId);
-
-      self.serial.disconnect(connId, function (ok) {
+    self.consumeMessage(responsePayloadSize, callback, function (error) {
+      self.log.error(error);
+      self.serial.disconnect(self.connectionId, function (ok) {
         if (ok) {
           self.connectionId = null;
-          self.log.log("Disconnected ok, You may now use your program!");
+          self.log.log("Disconnected ok");
         } else
           self.log.error("Could not disconnect from " + this.connectionId);
       });
-    else
-      if (callback) callback();
+    });
   });
 };
 
@@ -62,22 +72,68 @@ SerialTransaction.prototype.readToBuffer = function (readArg) {
   return false;
 };
 
-SerialTransaction.prototype.readByte = function (addr, byte, cb) {
-  var readop;
-  if (this.memOps.READ_LO)
-    if (addr % 2)
-      readop = this.memoryOps.readLow(addr);
+// Arguments like writheThenRead
+SerialTransaction.prototype.readByte = function (addr, cb) {
+  // XXX: We do not support TPI. See avrdude if you need it.
+
+  var readOp;
+  if (this.memOps.readLow) {
+    if (addr & 1)
+      readOp = this.memoryOps.readLow(addr/2);
     else
-      readop = this.memoryOps.readHigh(addr/2);
-  else {
-    readop = this.memoryOps.read(addr);
+      readOp = this.memoryOps.readHigh(addr/2);
+  } else {
+    readOp = this.memoryOps.read(addr);
   }
 
-  if (this.memOps.loadExtAddr)
-    this.writeThenRead_(this.memOps.loadExtAddr(addr));
+  var safeCmd = this.cmd.bind(this, readOp, function (ok, data) {
+    if (!ok) {
+      throw Error("Failed to send operation", readOp);
+    }
 
-  this.writeThenRead_(readop, 1, cb);
+    cb(ok, data);
+  });
+
+  if (this.memOps.loadExtAddr)
+    this.cmd(this.memOps.loadExtAddr(addr), function (ok, data) {
+      self.log.log("Ignoring extended addr read.");
+      if (ok) {
+        self.log.log("ExtAddr opcode success.");
+        safeCmd();
+      } else {
+        self.log.error("ExtAddr opcode failed. data: ", data);
+      }
+    });
+  else
+    safeCmd();
 };
 
+SerialTransaction.prototype.writeByte = function (data, addr, cb) {
+  // XXX: avrdude first reads to avoid writing if not necessary.
+  var writeOp, self = this;
+
+  if (this.memOps.writeLow) {
+    if (addr & 1)
+      writeOp = this.memoryOps.writeLow(addr/2, data);
+    else
+      writeOp = this.memoryOps.writeHigh(addr/2, data);
+  } else {
+    writeOp = this.memoryOps.write(addr);
+  }
+
+  self.cmd(writeOp, function (ok, data) {
+    if (!ok)
+      throw Error("Failed to send operation", writeOp);
+
+    // Check if we wrote the correct byte
+    poll(5, 250, function (tryAgain) {
+      self.readByte(addr, function (readData) {
+        if (readData & 0xff != data & 0xff) {
+          tryAgain();
+        }
+      });
+    });
+  });
+};
 
 module.exports = SerialTransaction;
