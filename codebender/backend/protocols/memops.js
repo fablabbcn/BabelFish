799afd20ff13e6ac87829267096f84a05bfcc18d
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2014-12-19 19:00:55 +0200

    First commit aboot memops.

--- codebender/backend/protocols/memops.js
+++ codebender/backend/protocols/memops.js
@@ -1,5 +1,40 @@
 // XXX: have the methods generate from avrdude.conf
 
+function OpcodeBit (id) {
+  this.id = id.slice(0,1);
+  this.index = Number(id.slice(1));
+  this.value = Number(this.id) || 0;
+}
+
+function OpcodeFactory(bitString) {
+  self.bits = bitString
+    .split(" ")
+    .filter(function (b) {return b.length != 0;})
+    .map(function (b) {return new OpcodeBit(b);});
+}
+
+OpcodeFactory.prototype = {
+  setBits: function (id, data) {
+    var array = [];
+    while (data) {
+      array.push(data & 1);
+      data >>= 1;
+    }
+
+    this.bits.
+      filter(function (b) {return b.id == id;})
+      .forEach(function (b) {
+        for (var i = 0 ; i < array.length; i++)
+          if (b.index == i)
+            b.value = data[i];
+    });
+  },
+
+  // XXX: Get the executable opcodes
+  getCmd: function () {
+  }
+};
+
 function MemoryOperations() {
 }
 
