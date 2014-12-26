9b6cf44aacb0e4014c6aecbb59cda493bc39e954
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2014-12-26 12:04:30 +0200

    Extra calls for chrome api.

---
+++ chrome-extension/host/ad-hoc.js
@@ -0,0 +1,11 @@
+// Nasty hacks that should be avoided.
+
+// Force a serial receive error: when we flash we need to force an
+// error event trigger.
+chrome.serial.onReceiveError.forceDispatch = function (info) {
+  hosts.serial.garbageCollectCallbacks();
+
+  hosts.serial.getListenerCallbacks('onReceiveError.addListener').forEach(function (cb) {
+    cb(info);
+  });
+};
