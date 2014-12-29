f691db96f08eb421050a8938c16be128769ee76d
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2014-12-30 00:46:27 +0200

    Dont check if you are fine

--- chrome-extension/common/config.js
+++ chrome-extension/common/config.js
@@ -46,9 +46,11 @@ var config = {
                    cleaner: 'onLaunched.removeListener'}]
     }
   }
-}, matchUrls=["http://localhost:8080/*",
-              "http://localhost/*",
-              "http://ec2-54-174-134-98.compute-1.amazonaws.com:8080/*"];
+},
+    matchUrls = ["http://localhost:8080/*",
+                 "http://localhost/*",
+                 "http://ec2-54-174-134-98.compute-1.amazonaws.com:8080/*"],
+    extensionSet;
 
 
 if (window.chrome && window.chrome.runtime && window.chrome.runtime.id)
@@ -57,32 +59,40 @@ if (window.chrome && window.chrome.runtime && window.chrome.runtime.id)
 // Send the extension id to the server to send correct config to the
 // client. Kind of async but we have a backup and we will make many
 // more requests to the server before useing the extensionId
-function updateExtensionId (url, id) {
+function updateExtensionId (urls, set) {
   var xhr = new XMLHttpRequest(),
-      ext = "extensionid";
+      ext = "extensionid",
+      url = urls.shift();
 
   // Define it if you are an extension
   if (window.chrome && window.chrome.runtime && chrome.runtime.id)
     ext += "?extensionid="+ chrome.runtime.id;
 
+  url = url.replace("*", ext);
   xhr.onreadystatechange = function () {
+    if (extensionSet) return;
     if (xhr.readyState == 4 &&
         xhr.status == 200 &&
-        xhr.responseText.length > 0)
+        xhr.responseText.length > 0) {
       config.extensionId = xhr.responseText;
+      console.log("Extension id is:", config.extensionId, "based on", url);
+      extensionSet = true;
+    } else {
+      console.log("Failed to get extension id from", url);
+      if (urls.length) updateExtensionId(urls);
+    }
 
-    console.log("Extension id is:", config.extensionId);
   };
 
   try {
-    xhr.open("GET", url.replace("*", ext), true);
-    xhr.send(null);
+    xhr.open("GET", url, true);
+    xhr.send();
   } catch (e) {
     ;
   }
 }
 
-matchUrls.forEach(function (url) { updateExtensionId(url);});
+updateExtensionId(matchUrls);
 
 try {
   module.exports = config;
