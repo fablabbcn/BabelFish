6fe795f12a5fefd11cc71d411a08b5e45f4cef45
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-02-05 11:52:48 +0200

    Moved essential transaction functions from serialtransaction to transaction

--- codebender/backend/protocols/usbtiny.js
+++ codebender/backend/protocols/usbtiny.js
@@ -3,18 +3,19 @@ var _create_chrome_client = require('./../../../chrome-extension/client/rpc-clie
     arraify = require('./../util').arraify,
     forEachWithCallback = require('./../util').forEachWithCallback,
     MemoryOperations = require('./memops'),
-    buffer = require("./../buffer.js");
+    buffer = require("./../buffer.js"),
+    log = new (require('./../logging').Log)('USBTiny');
 
-function USBTinyTransaction() {}
+function USBTinyTransaction() {
+  this.usb = chrome.usb;
+  this.log = log;
+}
 
 USBTinyTransaction.prototype = new Transaction();
 
-USBTinyTransaction.prototype.init = function (finishCallback, errorCallback) {
-
-};
-
 USBTinyTransaction.prototype.flash = function (devName, hexData) {
-  var self = this;
+  var self = this,
+      opts = {vendorId: 6017, productId: 3231};
 
   this.usb.findDevices(opts, function (hndls) {
     if (hndls.length == 0) {
@@ -30,5 +31,19 @@ USBTinyTransaction.prototype.flash = function (devName, hexData) {
 USBTinyTransaction.prototype.connectDone = function (hexCode) {
   var self = this;
 
-  console.log(self.handler);
+  log(self.handler);
+  self.transition('endTransaction');
 };
+
+USBTinyTransaction.prototype.connectDone = function () {
+  var self = this;
+
+  if (this.handler) {
+    self.usb.closeDevice(this.handler, function () {
+      log("Handler closed");
+      self.handler = null;
+    });
+  }
+};
+
+module.exports.USBTinyTransaction = USBTinyTransaction;
