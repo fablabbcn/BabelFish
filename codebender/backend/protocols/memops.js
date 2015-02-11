b888b09ded25aa8d204304f12134d58decc5d765
Chris Perivolaropoulos <darksaga2006@gmail.com>
2015-02-11 16:11:02 +0200

    Program enable for usbtiny

--- codebender/backend/protocols/memops.js
+++ codebender/backend/protocols/memops.js
@@ -1,76 +1,14 @@
-// XXX: have the methods generate from avrdude.conf
-
-// Reverse the bits in data
-function bitReverse (data, bits) {
-  var res = [];
-  bits = bits || 8;
-
-  for (var i=0; i < bits; i++)
-    res.push((data >> i) & 1);
-
-  return res.reverse()
-    .reduce(function (bit, ret) {return (ret << 1) | bit;}, 0);
-}
-
-// Split integer {data} into {bytes} bytes.
-function splitBytes (data, bytes, bigEndian) {
-  var ret = [];
-
-  for (var i = 0; i < bytes; i++) {
-    ret.push(data & 0xff);
-    data >>= 8;
-  }
+// A op to an array of bytes. No parameters.
+function opToBin(op) {
+  var ret = [0x0, 0x0, 0x0, 0x0];
+  op.forEach(function (bitStruct, index) {
+    var bit = bitStruct.instBit % 8,
+        byte = 3 - Math.floor(bitStruct.instBit / 8);
+    if (bitStruct.bitType == "VALUE")
+      ret[byte] |= bitStruct.value << bit;
+  });
 
   return ret;
 }
 
-
-function OpcodeBit (id) {
-  this.id = id.slice(0,1);
-  this.index = Number(id.slice(1));
-  this.value = Number(this.id) || 0;
-}
-
-function OpcodeFactory(bitString) {
-  self.bits = bitString
-    .split(" ")
-    .filter(function (b) {return b.length != 0;})
-    .map(function (b) {return new OpcodeBit(b);});
-}
-
-OpcodeFactory.prototype = {
-  setBits: function (id, data) {
-    var bitArray = [];
-    while (data) {
-      bitArray.push(data & 1);
-      data >>= 1;
-    }
-
-    // Set each bit object to a value.
-    this.bits.
-      filter(function (b) {return b.id == id;})
-      .forEach(function (b) {
-        for (var i = 0 ; i < bitArray.length; i++)
-          if (b.index == i)
-            b.value = data[i];
-      });
-  },
-
-  // Byte list of opcoes
-  getCmd: function () {
-    splitBytes(this.bits.reduce(function (val, bit) {
-      return (val << 1 | bit.value);
-    }));
-  }
-};
-
-function MemoryOperations() {
-}
-
-MemoryOperations.prototype = {
-  readLow: function(addr) {},
-  readHigh: function(addr) {},
-  read: function(addr) {}
-};
-
-module.exports = MemoryOperations;
+module.exports.opToBin = opToBin;
--- codebender/backend/protocols/usbtiny.js
+++ codebender/backend/protocols/usbtiny.js
@@ -3,11 +3,13 @@ var _create_chrome_client = require('./../../../chrome-extension/client/rpc-clie
     arraify = require('./../util').arraify,
     forEachWithCallback = require('./../util').forEachWithCallback,
     MemoryOperations = require('./memops'),
-    buffer = require("./../buffer.js"),
+    buffer = require("./../buffer"),
+    ops = require("./memops"),
     Log = require('./../logging').Log,
     log = new Log('USBTiny');
 
-function USBTinyTransaction() {
+function USBTinyTransaction(config, finishCallback, errorCallback) {
+  Transaction.apply(this, arraify(arguments));
   this.UT = {
     // Generic requests to the USBtiny
     ECHO: 0,              // echo test
@@ -51,27 +53,49 @@ USBTinyTransaction.prototype.transferOut = function (op, v1, v2, data) {
   };
 };
 
-USBTinyTransaction.prototype.transferIn = function (op, v1, v2, length) {
+USBTinyTransaction.prototype.transferIn = function (op, val, ind, length) {
   return {
     recipient: "device",
     direction: "in",
     requestType: "vendor",
     request: op,
-    value: v1,
-    index: v2,
+    value: val,
+    index: ind,
     length: length || 0
   };
 };
 
-USBTinyTransaction.prototype.control = function (op, v1, v2, cb) {
-  var self = this, transferInfo = self.transferIn(op, v1, v2);
-
-  self.usb.controlTransfer(
-    self.handler,
-    transferInfo, function (arg) {
+// Full fledged write with control
+USBTinyTransaction.prototype.write = function (info, cb) {
+  this.usb.controlTransfer(
+    this.handler,
+    info, function (arg) {
       arg.data = buffer.bufToBin(arg.data);
+
+      log.log(arg);
       cb(arg);
     });
+};
+
+// A simple control message with 2 values (index, value that is)
+USBTinyTransaction.prototype.control = function (op, v1, v2, cb) {
+  this.write(self.transferIn(op, v1, v2), cb);
+};
+
+USBTinyTransaction.prototype.cmd = function (cmd, cb) {
+  log.log("Sending command:", buffer.hexRep(cmd));
+
+  var info = this.transferIn(this.UT.SPI,
+                             (cmd[1] << 8) | cmd[0],
+                             (cmd[3] << 8) | cmd[2],
+                             4);
+
+  this.write(info, cb);
+};
+
+USBTinyTransaction.prototype.operation = function (op, cb) {
+  log.log("Running operation:", op);
+  return this.cmd(ops.opToBin(this.config.avrdude.ops[op]), cb);
 };
 
 // First argument is ignored for compatibility with serial flashes tha
@@ -91,27 +115,39 @@ USBTinyTransaction.prototype.flash = function (_, hexData) {
   });
 };
 
-USBTinyTransaction.prototype.powerUp = function () {
-  var self = this;
+USBTinyTransaction.prototype.powerUp = function (cb) {
+  log.log(this.handler);
+  this.control(this.UT.POWERUP, this.sck, this.UT.RESET_LOW,
+               cb || this.transitionCb('programEnable'));
+};
 
-  log.log(self.handler);
-  self.control(self.UT.POWERUP, self.sck, self.UT.RESET_LOW,
-               self.transitionCb('powerDown'));
+USBTinyTransaction.prototype.programEnable = function () {
+  this.operation("PGM_ENABLE", this.transitionCb('programPage', 0));
+};
+
+USBTinyTransaction.prototype.programPage = function (offset) {
+  var page = this.config.avrdude.memory.flash.page_size,
+      end = offset + page,
+      info = this.transferOut(this.UT.FLASH_READ, 0, offset,
+                              this.hexData.slice(offset, end));
+
+  if (end > this.hexData.length)
+    this.write(info, this.transitionCb("powerDown"));
+  else
+    this.transition('programPage', end);
 };
 
 USBTinyTransaction.prototype.powerDown = function (ctrlArg) {
-  var self = this;
-
-  log.log(self.handler);
-  self.control(self.UT.POWERDOWN, 0, 0,
-               self.transitionCb('endTransaction'));
+  log.log(this.handler);
+  this.control(this.UT.POWERDOWN, 0, 0,
+               this.transitionCb('endTransaction'));
 };
 
 USBTinyTransaction.prototype.endTransaction = function (ctrlArg) {
   var self = this;
 
   if (this.handler) {
-    self.usb.closeDevice(this.handler, function () {
+    this.usb.closeDevice(this.handler, function () {
       log.log("Handler closed");
       self.handler = null;
     });
