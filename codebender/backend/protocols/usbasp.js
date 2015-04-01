07a2c0f0381959aa65c0a13b5a95b2718222927d
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-04-01 10:54:47 +0300

    Sck setting.

--- codebender/backend/protocols/usbasp.js
+++ codebender/backend/protocols/usbasp.js
@@ -15,7 +15,27 @@ function USBAspTransaction(config, finishCallback, errorCallback) {
     SETLONGADDRESS: 0x0,
     READFLASH: 0x0,
     CAP_TPI: 0x01
+
+
   };
+
+  // The list of SCK frequencies in Hz supported by USBasp.
+  // Allowed freq -> id map
+  this.SCK_OPTIONS = {
+    1500000: 12,
+    750000: 11,
+    375000: 10,
+    187500: 9,
+    93750: 8,
+    32000: 7,
+    16000: 6,
+    8000: 5,
+    4000: 4,
+    2000: 3,
+    1000: 2,
+    500: 1
+  };
+
   this.cmdFunction = this.UA.TRANSMIT;
   this.entryState = 'checkCapabilities';
 };
@@ -35,8 +55,22 @@ USBAspTransaction.prototype.checkCapabilities = function () {
       return;
     }
 
-    setTimeout(this.transitionCb('programEnable'), 1000);
+    setTimeout(this.transitionCb('setSck'), 1000);
   });
+};
+
+USBAspTransaction.prototype.setSck = function () {
+  var request_hz = this.config,
+      sck_hz = Object.getPropertyNames(this.SCK_OPTIONS)
+        .map(Number)
+        .sort()
+        .filter(function (sck) {
+          return request_hz < sck;
+        })[0],
+      sck_id = this.SCK_OPTIONS[sck_hz],
+      info = this.transmitIn(this.UA.SETISPSCK, sck_id, 0, 4);
+
+  this.write(info, this.transitionCb("programEnable"));
 };
 
 USBAspTransaction.prototype.programEnable = function () {
