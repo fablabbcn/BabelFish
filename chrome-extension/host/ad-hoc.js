9eb5b413bae34a1e34890e4e3fce7637a2211b8a
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-04-20 17:45:22 +0300

    Custom serial onReceive listener that sends packets in bursts to not clog the messaging system.

--- chrome-extension/host/ad-hoc.js
+++ chrome-extension/host/ad-hoc.js
@@ -1,4 +1,5 @@
 // Nasty hacks that should be avoided.
+chrome.adhoc = {};
 
 // Force a serial receive error: when we flash we need to force an
 // error event trigger.
@@ -12,6 +13,50 @@ chrome.serial.onReceiveError.forceDispatch = function (info) {
 };
 
 chrome.runtime.getManifestAsync = function (cb) {
-    var manifest = chrome.runtime.getManifest();
-    cb(manifest);
+  var manifest = chrome.runtime.getManifest();
+  cb(manifest);
+};
+
+// Aggregate calls in a packet and send if an interval is provided.
+// ATTENTION: in order for chrome message passing to JSONify the
+// callback arguments we return arrays instead of arraybuffers in
+// readArg.data.
+var realReceiveListener = chrome.serial.onReceive.addListener.bind(chrome.serial.onReceive);
+chrome.serial.onReceive.addListener = function (cb, interval) {
+  var args = [], timeout = null;
+  function callback() {
+    var cbargs = [].slice.call(arguments);
+    return cb.apply(null, cbargs);
+  }
+
+  // Fallback for backwards compatibility.
+  if (!interval) {
+    realReceiveListener(callback);
+    return;
+  }
+
+  function storeArg (readArg) {
+    if (args.length > 100){
+      throw Error("Too many args:", args.length);
+    }
+
+    var bufferView = new Uint8Array(readArg.data);
+    readArg.data = [].slice.call(bufferView);
+    var cbargs = [].slice.call(arguments);
+
+    args = args.concat([cbargs]);
+    if (timeout === null) {
+      timeout = setTimeout(flush, interval);
+    }
+  }
+
+  function flush() {
+    timeout = null;
+    if (args.length != 0) {
+      callback(args);
+      args = [];
+    }
+  }
+
+  realReceiveListener(storeArg);
 };
