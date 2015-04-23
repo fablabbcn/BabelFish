8cd072312d767668ff3adafc6f137b9089a8280b
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-04-23 21:28:37 +0300

    Better pull requests (ask the user about your decisions)

--- build/browserify.mk
+++ build/browserify.mk
@@ -39,11 +39,30 @@ browserify-twig:
 log-since-push:
 	PAGER=cat git log "$(shell head $(codebender-twig-dir)/chrome-client.js.twig | sed -n 's_// Commit: \(.*\)_\1_p')..HEAD"
 
-pull-request: browserify-twig
-	git -C $(CODEBENDER_CC) commit -a -m '$(shell $(MAKE) log-since-push)' || echo "===== Nothing to commit ====="
+y-or-n = read -n 1 -p "==== Are you ok with this (y/n)? " answer && [[ "$$answer" = "y" ]]
+cbgit = PAGER=cat git -C $(CODEBENDER_CC)
+pull-request-branch = babelfish_$(shell git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads/ | grep -v 'master' | sed 's_refs/heads/\(.*\)_\1_p' | head -1)
+.PHONY:
+setup-pull-branch:
+	$(cbgit) checkout development
+	$(cbgit) pull origin development
+	@echo "--------"
+	@echo "Will create branch named '$(pull-request-branch)' (set pull-request-branch to override)"
+	$(y-or-n)
+	$(cbgit) checkout -b "$(pull-request-branch)"
+
+.PHONY:
+pull-request: setup-pull-branch browserify-twig
+	$(cbgit) commit -a -m '$(shell $(MAKE) log-since-push)' || \
+		(echo "===== Nothing to commit =====" && false)
+	@echo "You can push with: make -C '$(dot)' pull-request-branch=$(pull-request-branch) push-pull-request"
+
 
 push-pull-request:
-	git -C $(CODEBENDER_CC) push
+	$(cbgit) diff development
+	@echo "Will now push..."
+	$(y-or-n)
+	$(cbgit) push origin $(pull-request-branch)
 
 $(codebender-twig-dir)/%.twig: $(dot)/bundles/%
 	@echo "Generating: $@"
