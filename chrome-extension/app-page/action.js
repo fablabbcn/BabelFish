6b9c0252d6a56fe3097af0af5f5334a5b625eb1f
Pral2a <guillem@pral2a.com>
2015-07-15 19:31:04 +0200

    About changes

---
+++ chrome-extension/app-page/action.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+  if(navigator.platform.indexOf("Win") != -1) document.querySelector(".windows-drivers").style.display='block';
+}, false);
--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('app-page/index.html', {
     'width': 420,
-    'height': 620
+    'height': 660
   });
 });
